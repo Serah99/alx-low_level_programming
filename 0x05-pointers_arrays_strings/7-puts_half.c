115ff6b0abe7b90d3701a4fbc2ea55bf2ce8bc9d
Sarah <sarahaku12@gmail.com>
2023-08-21 14:29:22 -0700

    sss

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,31 +2,19 @@
 
 /**
  * puts_half - Prints the second half of a string.
- * @str: The string to be processed.
- *
- * Description: If the string length is odd, it prints the second half starting
- * from (length - 1) / 2. Otherwise, it starts from length / 2. The output is 
- * followed by a newline.
+ * @str: The string.
  */
 void puts_half(char *str)
 {
-	int index, len_half, len_full;
+	int len = 0, n;
 
-	len_full = 0;
+	while (str[len])
+		len++;
 
-	// Calculate the string length
-	for (index = 0; str[index] != '\0'; index++)
-		len_full++;
+	n = (len % 2) ? (len + 1) / 2 : len / 2;
 
-	// Determine starting point for printing
-	if (len_full % 2 == 0)
-		len_half = len_full / 2;
-	else
-		len_half = (len_full + 1) / 2;
-
-	// Print the second half of the string
-	for (index = len_half; str[index] != '\0'; index++)
-		_putchar(str[index]);
+	for (; str[n]; n++)
+		_putchar(str[n]);
 
 	_putchar('\n');
 }
