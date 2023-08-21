a613613e43d9d2a0941a06d2513cc429fab204e9
Sarah <sarahaku12@gmail.com>
2023-08-21 13:37:45 -0700

    gggg

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 void reset_to_98(int *n) {
-    if (n != NULL) { // Check to ensure the pointer is not NULL
-        *n = 98;     // Update the value it points to
+    if (n != NULL) { /* Check to ensure the pointer is not NULL */
+        *n = 98;     /* Update the value it points to */
     }
 }
 
@@ -10,7 +10,7 @@ int main() {
     int x = 0;
     printf("Before: %d\n", x);
     
-    reset_to_98(&x);  // Pass the address of x to the function
+    reset_to_98(&x);  /* Pass the address of x to the function */
     
     printf("After: %d\n", x);
 
