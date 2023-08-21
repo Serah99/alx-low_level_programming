92d01f1c89a0ec1119e7c9db8345fa870166f258
Sarah <sarahaku12@gmail.com>
2023-08-21 13:47:50 -0700

    fff

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,5 @@
-/**
- * reset_to_98 - Updates the value it points to to 98.
- * @n: A pointer to an int that will be updated.
- *
- * Return: void.
- */
+#include <stdio.h>
+
 void reset_to_98(int *n) {
     if (n != NULL) { /* Check to ensure the pointer is not NULL */
         *n = 98;     /* Update the value it points to */
