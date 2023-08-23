7f857cb159bc2e37e4706c7dda334dd38f00ad28
Sarah <sarahaku12@gmail.com>
2023-08-22 22:59:30 -0700

    aaa

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+char *_strcat(char *d, char *s)
+{
+    char *orig_d = d;
+
+    while (*d)
+    {
+        d++;
+    }
+
+    while (*s)
+    {
+        *d = *s;
+        d++;
+        s++;
+    }
+
+    *d = '\0';
+    return orig_d;
+}
+
+int main(void)
+{
+    char d[100] = "Hello, ";
+    char s[] = "World!";
+    printf("%s\n", _strcat(d, s));
+    return 0;
+}
