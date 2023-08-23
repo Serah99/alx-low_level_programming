f324739658b666fdacfcd30ee098b8d4fd06af8a
Sarah <sarahaku12@gmail.com>
2023-08-23 11:48:09 -0700

    kkkkk

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - encode into 1337speak
+ * @s: input value
+ * Return: s value
+ */
+char *leet(char *s)
+{
+	int x, y;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (x = 0; s[x]; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (s[x] == a[y])
+				s[x] = b[y];
+		}
+	}
+	return s;
+}
