1262046bcd7d685a89c8515b6b2cac47e9bc3ed1
root <root@DESKTOP-IB29KRK.localdomain>
2023-09-13 19:29:35 +0300

    sssss

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,22 +1,21 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-
+#ifndef CALC_H
+#define CALC_H
 /**
- * struct operation - A structure for operation.
- * @symbol: The arithmetic symbol.
- * @func: The function associated with the operation.
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
  */
-typedef struct operation
+typedef struct op
 {
-	char *symbol;
-	int (*func)(int x, int y);
-} operation_t;
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
-int op_add(int x, int y);
-int op_sub(int x, int y);
-int op_mul(int x, int y);
-int op_div(int x, int y);
-int op_mod(int x, int y);
-int (*get_op_func(char *oper))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif
