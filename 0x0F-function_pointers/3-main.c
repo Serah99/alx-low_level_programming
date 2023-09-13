293656acae221ad9408ea83d25e2e32cbf3ca722
root <root@DESKTOP-IB29KRK.localdomain>
2023-09-13 19:27:23 +0300

    ssss

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,43 +2,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
-
 /**
- * main - Prints the outcome of basic arithmetic operations.
- * @arg_num: The quantity of arguments provided to the program.
- * @arg_values: An array of pointers to the arguments.
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: Always 0.
  */
-int main(int __attribute__((__unused__)) arg_num, char *arg_values[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int value1, value2;
-	char *operator;
+	int num1, num2;
+	char *op;
 
-	if (arg_num != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	value1 = atoi(arg_values[1]);
-	operator = arg_values[2];
-	value2 = atoi(arg_values[3]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*operator == '/' && value2 == 0) ||
-	    (*operator == '%' && value2 == 0))
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	printf("%d\n", get_op_func(operator)(value1, value2));
-
-	return (0);
-}
