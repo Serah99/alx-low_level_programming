b52155745010ee547510d98ca8d3281010d8b179
root <root@DESKTOP-IB29KRK.localdomain>
2023-09-13 19:19:44 +0300

    sss

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- * array_printer - prints elements of an array based on given action
- * @arr: the array to be processed
- * @len: number of elements in the array
- * @callback: function pointer to process each element
- * Return: none
+ * array_iterator - prints each array element on a newline
+ * @arr: array of integers
+ * @length: number of elements to process
+ * @callback: pointer to a function that processes each element
+ * Return: void
  */
-void array_printer(int *arr, size_t len, void (*callback)(int))
+void array_iterator(int *arr, size_t length, void (*callback)(int))
 {
 	unsigned int idx;
 
 	if (arr == NULL || callback == NULL)
 		return;
 
-	for (idx = 0; idx < len; idx++)
+	for (idx = 0; idx < length; idx++)
 	{
 		callback(arr[idx]);
 	}
