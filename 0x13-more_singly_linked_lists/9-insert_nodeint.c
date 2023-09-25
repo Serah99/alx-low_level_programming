e600791d26cacc713620844b396264789f8d2a03
Sarah <sarahaku12@gmail.com>
2023-09-25 16:49:27 -0700

    ffffff

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,45 @@
 #include "lists.h"
 
 /**
-* insert_nodeint_at_position - Inserts a new node to a listint_t
-*                              list at a specified position.
-* @head: A pointer to the address of the
-*        beginning of the listint_t list.
-* @idx: The index in the listint_t list where the new
-*       node should be placed - indices begin at 0.
-* @n: The integer for the new node to hold.
-*
-* Return: If the function fails - NULL.
-*         Otherwise - the address of the newly added node.
-*/
-listint_t *insert_nodeint_at_position(listint_t **head, unsigned int idx, int n)
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new_node, *temp = *head;
-unsigned int count;
+	unsigned int counter;
+	listint_t *newNode;
+	listint_t *currentNode = *head;
 
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-return (NULL);
+	newNode = malloc(sizeof(listint_t));
+	if (!newNode || !head)
+		return (NULL);
 
-new_node->n = n;
+	newNode->n = n;
+	newNode->next = NULL;
 
-if (idx == 0)
-{
-new_node->next = temp;
-*head = new_node;
-return (new_node);
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+
+	for (counter = 0; currentNode && counter < idx; counter++)
+	{
+		if (counter == idx - 1)
+		{
+			newNode->next = currentNode->next;
+			currentNode->next = newNode;
+			return (newNode);
+		}
+		else
+			currentNode = currentNode->next;
 }
 
-for (count = 0; count < (idx - 1); count++)
-{
-if (temp == NULL || temp->next == NULL)
 return (NULL);
-
-temp = temp->next;
-}
-
-new_node->next = temp->next;
-temp->next = new_node;
-
-return (new_node);
 }
