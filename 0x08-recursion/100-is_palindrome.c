5bca3fc693c553e04ebf2219c0a32d8c772ef0d6
Sarah <sarahaku12@gmail.com>
2023-09-26 15:02:43 -0700

    kkkkkk

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,47 @@
 #include "main.h"
 
-int verify_palindrome(char *str, int idx, int length);
-int string_length_recursive(char *str);
+int verify_palindrome(char *str, int index, int length);
+int compute_strlen_recursive(char *str);
 
 /**
- * evaluate_palindrome - checks if a string is a palindrome
+ * is_palindrome - checks if a string is a palindrome
  * @str: string to evaluate
  *
- * Return: 1 if it is a palindrome, 0 if it's not
+ * Return: 1 if it is a palindrome, 0 if not
  */
-int evaluate_palindrome(char *str)
+int is_palindrome(char *str)
 {
 	if (*str == 0)
 		return (1);
-	return (verify_palindrome(str, 0, string_length_recursive(str)));
+	return (verify_palindrome(str, 0, compute_strlen_recursive(str)));
 }
 
 /**
- * string_length_recursive - returns the length of a string
- * @str: string to calculate the length of
+ * compute_strlen_recursive - returns the length of a string
+ * @str: string to determine the length of
  *
  * Return: length of the string
  */
-int string_length_recursive(char *str)
+int compute_strlen_recursive(char *str)
 {
 	if (*str == '\0')
 		return (0);
-	return (1 + string_length_recursive(str + 1));
+	return (1 + compute_strlen_recursive(str + 1));
 }
 
 /**
- * verify_palindrome - checks the characters recursively for palindrome
- * @str: string to check
- * @idx: iterator
+ * verify_palindrome - checks characters recursively for palindrome
+ * @str: string to verify
+ * @index: iterator
  * @length: length of the string
  *
  * Return: 1 if palindrome, 0 if not
  */
-int verify_palindrome(char *str, int idx, int length)
+int verify_palindrome(char *str, int index, int length)
 {
-	if (*(str + idx) != *(str + length - 1))
+	if (*(str + index) != *(str + length - 1))
 		return (0);
-	if (idx >= length)
+	if (index >= length)
 		return (1);
-	return (verify_palindrome(str, idx + 1, length - 1));
+	return (verify_palindrome(str, index + 1, length - 1));
 }
